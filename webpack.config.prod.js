fa22a275bc67aacf36670cafb0c44b6e01e3500e
ymll <yml.lily@gmail.com>
2024-05-01 08:27:27 -0700

    Add Google Analytics partial

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,8 @@ const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
+const path = require('path');
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 
@@ -11,6 +13,14 @@ module.exports = merge(common, {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.join(__dirname, './partials/analytics.html'),
+      location: 'head',
+      priority: 'low',
+      options: {
+        ga_property_id: 'G-XQ255X4GQ2'
+      }
     }),
     new CopyPlugin({
       patterns: [
