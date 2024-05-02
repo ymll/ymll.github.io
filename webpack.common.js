1cb1a7bd6ef772381e885a196d395fc21e771427
ymll <ymll@users.noreply.github.com>
2024-05-02 16:37:30 +0800

    Improve deployment of static files
    - Deploy images with content hash filename
    - Bundle and minify multiple CSS files
    - Generate more HTML with HtmlWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
@@ -7,6 +9,24 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
+    filename: 'app.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.html/i,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
